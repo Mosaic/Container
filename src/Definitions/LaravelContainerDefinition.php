f0e3880fa1cacdd2281cd8f7e2b03b240c6c1108
Patrick Brouwers <patrickbrouwersfilm@gmail.com>
2016-03-06 15:13:02 +0100

    Fix

--- src/Definitions/LaravelContainerDefinition.php
+++ src/Definitions/LaravelContainerDefinition.php
@@ -4,8 +4,8 @@ namespace Mosaic\Container\Definitions;
 
 use Illuminate\Container\Container;
 use Mosaic\Container\Adapters\Laravel\Container as Adapter;
-use Mosaic\Contracts\Container\Container as ContainerContract;
-use Mosaic\Contracts\Container\ContainerDefinition;
+use Mosaic\Container\Container as ContainerContract;
+use Mosaic\Container\ContainerDefinition;
 
 class LaravelContainerDefinition implements ContainerDefinition
 {
